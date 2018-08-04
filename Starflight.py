d68aec932192925f9e6644b1b5c527c1885b6d71
Unknown <dragonofawsome@gmail.com>
2018-08-04 14:27:14 -0400

    Created Ship and Power objects for ease of access

--- Starflight.py
+++ Starflight.py
@@ -1,15 +1,11 @@
-import os
 import msvcrt
+import os
 from datetime import datetime
+from pprint import pformat
 
 hull = 100
 shld = 100
 shldstatus = 'Lowered'
-engp = 3
-weap = 3
-shep = 3
-auxp = 3
-resp = 0
 engpa = ''
 weapa = ''
 shepa = ''
@@ -56,12 +52,36 @@ def print_menu(lines, min_value, max_value):
     return value
 
 
+class Power:
+    def __init__(self, **kwargs):
+        self.engines = 3
+        self.weapons = 3
+        self.shields = 3
+        self.auxiliary = 3
+        self.reserve = 3
+
+
+class Ship:
+    """Ship objects contain information about a ship,
+    whether it's the player, an enemy, or an NPC."""
+
+    def __init__(self, name=None, owner=None, **kwargs):
+        self.name = str(name)
+        self.model = None
+        self.owner = str(owner)
+        self.hull = 100
+        self.shields = 100
+        self.power = Power()
+
+
 def mainMenu():
-    global engp, weap, shep, auxp, resp, engpa, weapa, shepa, auxpa, respa
+    global engpa, weapa, shepa, auxpa, respa
+    log('Switched to main menu')
+    log('Player ship status: ' + pformat(pShip))
     while True:
         cls()
         power_display()
-        print('USS Starflight')
+        print(pShip.name + ' - Main Menu')
         print('')
         print('')
         print('Ship Status:')
@@ -86,8 +106,13 @@ def mainMenu():
                 i = 'Raise'
             print('2: %s shields' % i)
         # TODO: Add ValueError catch to handle non-numeric keypresses
-        c = int(msvcrt.getch())
-        log(c)
+
+        c = 0
+        try:
+            # c = int(msvcrt.getch())
+            c = int(input())
+        except ValueError:
+            pass
         if c is 1:
             powerMenu()
         elif c is 2 and shldstatus is not 'Disabled':
@@ -95,21 +120,21 @@ def mainMenu():
 
 
 def power_display():
-    global engp, weap, shep, auxp, resp, engpa, weapa, shepa, auxpa, respa
+    global engpa, weapa, shepa, auxpa, respa
     engpa = ''
     weapa = ''
     shepa = ''
     auxpa = ''
     respa = ''
-    for i in range(engp):
+    for i in range(pShip.power.engines):
         engpa += '■'
-    for i in range(weap):
+    for i in range(pShip.power.weapons):
         weapa += '■'
-    for i in range(shep):
+    for i in range(pShip.power.shields):
         shepa += '■'
-    for i in range(auxp):
+    for i in range(pShip.power.auxiliary):
         auxpa += '■'
-    for i in range(resp):
+    for i in range(pShip.power.reserve):
         respa += '■'
 
 
@@ -122,11 +147,13 @@ def toggle_shields():
 
 
 def powerMenu():
-    global engp, weap, shep, auxp, resp, engpa, weapa, shepa, auxpa, respa
+    log('Switched to power management menu')
+    log('Player ship status: ' + pformat(pShip))
+    global engpa, weapa, shepa, auxpa, respa
     while True:
         cls()
         power_display()
-        print('USS Starflight')
+        print(pShip.name + ' - Power Management')
         print('')
         print('')
         print('Power Levels:')
@@ -150,41 +177,50 @@ def powerMenu():
         print('8: Decrease power to Auxiliary')
         print('')
         print('9: Exit menu')
-        c = int(msvcrt.getch())
+
+        c = 0
+        try:
+            # c = int(msvcrt.getch())
+            c = int(input())
+        except ValueError:
+            pass
+
         if c is 1:
-            if resp > 0:
-                engp += 1
-                resp -= 1
+            if pShip.power.reserve > 0:
+                pShip.power.engines += 1
+                pShip.power.reserve -= 1
         elif c is 5:
-            if engp > 0:
-                resp += 1
-                engp -= 1
+            if pShip.power.engines > 0:
+                pShip.power.reserve += 1
+                pShip.power.engines -= 1
         elif c is 2:
-            if resp > 0:
-                weap += 1
-                resp -= 1
+            if pShip.power.reserve > 0:
+                pShip.power.weapons += 1
+                pShip.power.reserve -= 1
         elif c is 6:
-            if weap > 0:
-                resp += 1
-                weap -= 1
+            if pShip.power.weapons > 0:
+                pShip.power.reserve += 1
+                pShip.power.weapons -= 1
         elif c is 3:
-            if resp > 0:
-                shep += 1
-                resp -= 1
+            if pShip.power.reserve > 0:
+                pShip.power.shields += 1
+                pShip.power.reserve -= 1
         elif c is 7:
-            if shep > 0:
-                resp += 1
-                shep -= 1
+            if pShip.power.shields > 0:
+                pShip.power.reserve += 1
+                pShip.power.shields -= 1
         elif c is 4:
-            if resp > 0:
-                auxp += 1
-                resp -= 1
+            if pShip.power.reserve > 0:
+                pShip.power.auxiliary += 1
+                pShip.power.reserve -= 1
         elif c is 8:
-            if auxp > 0:
-                resp += 1
-                auxp -= 1
+            if pShip.power.auxiliary > 0:
+                pShip.power.reserve += 1
+                pShip.power.auxiliary -= 1
         elif c is 9:
             return
 
 
+p = Power()
+pShip = Ship('USS Starflight', 'Player', power=p)
 mainMenu()
