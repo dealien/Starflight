5008f97ed93e69ae05047cdb635a48bf7b6ba26e
Unknown <dragonofawsome@gmail.com>
2018-08-05 20:40:15 -0400

    Refactored pShip variable name to pship

--- Starflight.py
+++ Starflight.py
@@ -66,15 +66,15 @@ class Ship:
         shepa = ''
         auxpa = ''
         respa = ''
-        for i in range(pShip.power.engines):
+        for i in range(pship.power.engines):
             engpa += '■'
-        for i in range(pShip.power.weapons):
+        for i in range(pship.power.weapons):
             weapa += '■'
-        for i in range(pShip.power.shields):
+        for i in range(pship.power.shields):
             shepa += '■'
-        for i in range(pShip.power.auxiliary):
+        for i in range(pship.power.auxiliary):
             auxpa += '■'
-        for i in range(pShip.power.reserve):
+        for i in range(pship.power.reserve):
             respa += '■'
         return engpa, weapa, shepa, auxpa, respa
 
@@ -112,21 +112,21 @@ def save_game(name, data):
 def load_game(name):
     with open('saves/' + name + '.txt') as json_file:
         data = json.load(json_file)
-        pShip.load(data)
+        pship.load(data)
 
 
 def mainmenu():
     log('Switched to main menu')
-    log('Player ship status: ' + pformat(pShip))
+    log('Player ship status: ' + pformat(pship))
     while True:
         cls()
-        engpa, weapa, shepa, auxpa, respa = pShip.power_display()
-        print(pShip.name + ' - Main Menu')
+        engpa, weapa, shepa, auxpa, respa = pship.power_display()
+        print(pship.name + ' - Main Menu')
         print('')
         print('')
         print('Ship Status:')
-        print('Hull:      %s%%' % pShip.hull)
-        print('Shields:   %s%% (%s)' % (pShip.shields, pShip.shieldstatus))
+        print('Hull:      %s%%' % pship.hull)
+        print('Shields:   %s%% (%s)' % (pship.shields, pship.shieldstatus))
         print('')
         print('Power Levels:')
         print('Engines   : %s' % engpa)
@@ -139,8 +139,8 @@ def mainmenu():
         print('')
         print('')
         print('1: Power settings')
-        if pShip.shieldstatus is not 'Disabled':
-            if pShip.shieldstatus is 'Raised':
+        if pship.shieldstatus is not 'Disabled':
+            if pship.shieldstatus is 'Raised':
                 i = 'Lower'
             else:
                 i = 'Raise'
@@ -153,17 +153,17 @@ def mainmenu():
             pass
         if c is 1:
             powermenu()
-        elif c is 2 and pShip.shieldstatus is not 'Disabled':
-            pShip.toggle_shields()
+        elif c is 2 and pship.shieldstatus is not 'Disabled':
+            pship.toggle_shields()
 
 
 def powermenu():
     log('Switched to power management menu')
-    log('Player ship status: ' + pformat(pShip))
+    log('Player ship status: ' + pformat(pship))
     while True:
         cls()
-        engpa, weapa, shepa, auxpa, respa = pShip.power_display()
-        print(pShip.name + ' - Power Management')
+        engpa, weapa, shepa, auxpa, respa = pship.power_display()
+        print(pship.name + ' - Power Management')
         print('')
         print('')
         print('Power Levels:')
@@ -195,45 +195,45 @@ def powermenu():
             pass
 
         if c is 1:
-            if pShip.power.reserve > 0:
-                pShip.power.engines += 1
-                pShip.power.reserve -= 1
+            if pship.power.reserve > 0:
+                pship.power.engines += 1
+                pship.power.reserve -= 1
         elif c is 5:
-            if pShip.power.engines > 0:
-                pShip.power.reserve += 1
-                pShip.power.engines -= 1
+            if pship.power.engines > 0:
+                pship.power.reserve += 1
+                pship.power.engines -= 1
         elif c is 2:
-            if pShip.power.reserve > 0:
-                pShip.power.weapons += 1
-                pShip.power.reserve -= 1
+            if pship.power.reserve > 0:
+                pship.power.weapons += 1
+                pship.power.reserve -= 1
         elif c is 6:
-            if pShip.power.weapons > 0:
-                pShip.power.reserve += 1
-                pShip.power.weapons -= 1
+            if pship.power.weapons > 0:
+                pship.power.reserve += 1
+                pship.power.weapons -= 1
         elif c is 3:
-            if pShip.power.reserve > 0:
-                pShip.power.shields += 1
-                pShip.power.reserve -= 1
+            if pship.power.reserve > 0:
+                pship.power.shields += 1
+                pship.power.reserve -= 1
         elif c is 7:
-            if pShip.power.shields > 0:
-                pShip.power.reserve += 1
-                pShip.power.shields -= 1
+            if pship.power.shields > 0:
+                pship.power.reserve += 1
+                pship.power.shields -= 1
         elif c is 4:
-            if pShip.power.reserve > 0:
-                pShip.power.auxiliary += 1
-                pShip.power.reserve -= 1
+            if pship.power.reserve > 0:
+                pship.power.auxiliary += 1
+                pship.power.reserve -= 1
         elif c is 8:
-            if pShip.power.auxiliary > 0:
-                pShip.power.reserve += 1
-                pShip.power.auxiliary -= 1
+            if pship.power.auxiliary > 0:
+                pship.power.reserve += 1
+                pship.power.auxiliary -= 1
         elif c is 9:
             return
 
 
 p = Power()
-pShip = Ship('USS Starflight', 'Player', power=p)
-print(pShip.dump)
-save_game(username, pShip.dump)
+pship = Ship('USS Starflight', 'Player', power=p)
+print(pship.dump)
+save_game(username, pship.dump)
 load_game(username)
-print(pShip.dump)
+print(pship.dump)
 mainmenu()
